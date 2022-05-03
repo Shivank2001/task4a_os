55b91a5727f298999d2fd171bbc6630c9a7dfc87
Shivank <jinga2001@gmail.com>
2022-05-03 22:18:36 +0530

    task4

--- main/main.c
+++ main/main.c
@@ -5,7 +5,7 @@
 
 void app_main(void *pvParams)
 {
-    xTaskCreatePinnedToCore(&led_blink1,"LED_BLINK1",10000,NULL,1,NULL,0);
+    xTaskCreatePinnedToCore(&led_blink1,"LED_BLINK1",10000,NULL,1,NULL,1);
     xTaskCreatePinnedToCore(&led_blink2,"LED_BLINK2",10000,NULL,1,NULL,1);
 
 
